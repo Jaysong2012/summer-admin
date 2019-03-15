a1b57ebba2128363c7c7aaf02d6240940ac97a83
songchao <740596967@qq.com>
2019-03-15 15:31:19 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import './styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import i18n from './lang' // Internationalization
+import './icons' // icon
+import './permission' // permission control
+
+Vue.config.productionTip = false
+
+import * as filters from './filters' // global filters
+
+Vue.use(ElementUI, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
+})
+
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
+new Vue({
+  router,
+  store,
+  i18n,
+  render: h => h(App),
+}).$mount('#app')
